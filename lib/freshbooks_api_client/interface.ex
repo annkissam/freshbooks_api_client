7ce0198f33c6847ae359af9cb883fdecf3c406dc
Adi <aditya7iyengar@gmail.com>
2018-01-22 15:49:24 -0500

    Update interface behaviour

--- lib/freshbooks_api_client/interface.ex
+++ lib/freshbooks_api_client/interface.ex
@@ -12,6 +12,10 @@ defmodule FreshbooksApiClient.Interface do
   * list(params) -> Retrieves a list of existing resources from Freshbooks.
   """
 
+  alias FreshbooksApiClient.Caller.{HttpXml, InMemory}
+
+  @actions ~w(create update get delete list)a
+
   @typedoc ~S(Types that denote a successful response)
   @type success :: HTTPoison.Response.t()
 
@@ -21,20 +25,18 @@ defmodule FreshbooksApiClient.Interface do
   @typedoc ~S(Various response types that can happen upon an interface call)
   @type response :: {:ok, success()} | {:error, failure()}
 
-  @callback create(map) :: response()
-  @callback update(map) :: response()
-  @callback get(map) :: response()
-  @callback delete(map) :: response()
-  @callback list(map) :: response()
+  @callback create(atom(), map) :: response()
+  @callback update(atom(), map) :: response()
+  @callback get(atom(), map) :: response()
+  @callback delete(atom(), map) :: response()
+  @callback list(atom(), map) :: response()
 
-  @doc ~S(A Simple way of accessing all Schema's features)
+  @doc ~S(A Simple way of accessing all of Interace's features)
   defmacro __using__(opts) do
     schema = Keyword.get(opts, :schema)
+    allowed = Keyword.get(opts, :allow, @actions)
 
     quote do
-      use Ecto.Schema
-      import unquote(__MODULE__)
-
       @behaviour unquote(__MODULE__)
 
       defp schema() do
@@ -43,6 +45,53 @@ defmodule FreshbooksApiClient.Interface do
           _ -> unquote(schema)
         end
       end
+
+      def create(caller, params) do
+        case Enum.member?(unquote(allowed), :create) do
+          true ->
+            method = apply(unquote(schema), :resource, []) <> ".create"
+            apply(caller, :run, [method: method, params: params])
+          _ -> raise "action `:create` not allowed for #{unquote(schema)}"
+        end
+      end
+
+      def update(caller, params) do
+        case Enum.member?(unquote(allowed), :update) do
+          true ->
+            method = apply(unquote(schema), :resource, []) <> ".update"
+            apply(caller, :run, [method: method, params: params])
+          _ -> raise "action `:update` not allowed for #{unquote(schema)}"
+        end
+      end
+
+      def get(caller, params) do
+        case Enum.member?(unquote(allowed), :get) do
+          true ->
+            method = apply(unquote(schema), :resource, []) <> ".get"
+            apply(caller, :run, [method: method, params: params])
+          _ -> raise "action `:get` not allowed for #{unquote(schema)}"
+        end
+      end
+
+      def delete(caller, params) do
+        case Enum.member?(unquote(allowed), :delete) do
+          true ->
+            method = apply(unquote(schema), :resource, []) <> ".delete"
+            apply(caller, :run, [method: method, params: params])
+          _ -> raise "action `:delete` not allowed for #{unquote(schema)}"
+        end
+      end
+
+      def list(caller, params) do
+        case Enum.member?(unquote(allowed), :list) do
+          true ->
+            method = apply(unquote(schema), :resource, []) <> ".list"
+            apply(caller, :run, [method: method, params: params])
+          _ -> raise "action `:list` not allowed for #{unquote(schema)}"
+        end
+      end
+
+      defoverridable Enum.map(unquote(allowed), &{&1, 2})
     end
   end
 end
