363162f6ce0d51b9c44530ad2c591f5cc3812af3
Adi <aditya7iyengar@gmail.com>
2018-01-22 20:36:53 -0500

    Update Xml to use XmlBuilder for requests

--- lib/freshbooks_api_client/xml.ex
+++ lib/freshbooks_api_client/xml.ex
@@ -4,42 +4,29 @@ defmodule FreshbooksApiClient.Xml do
   to readble params
   """
 
-  @base """
-  <?xml version="1.0", encoding="utf-8"?>
-  <request method=":method">
-    <:resource>
-      :fields
-    </:resource>
-  </request>
-  """
+  import XmlBuilder
 
-  @field "<:field>:value</:field>"
+  @posts ~w(create update)
 
   def to_xml(method, params) do
-    resource = get_resource(method)
+    {resource, method} = get_resource_and_method(method)
     get_xml_string(resource, method, Enum.to_list(params))
   end
 
-  defp get_resource(method), do: method |> String.split(".") |> Enum.at(0)
+  defp get_resource_and_method(method) do
+    [resource, method] = String.split(method, ".")
+    {resource, method}
+  end
 
+  defp get_xml_string(resource, method, keyword) when method in @posts do
+    :request
+    |> element(%{method: resource <> "." <> method},
+               [element(resource, keyword)])
+    |> doc()
+  end
   defp get_xml_string(resource, method, keyword) do
-    fields = keyword
-      |> Enum.reduce("", &accumulate_fields/2)
-      |> String.trim("\n")
-
-    @base
-    |> String.replace(":resource", resource)
-    |> String.replace(":method", method)
-    |> String.replace(":fields", fields)
-  end
-
-  defp accumulate_fields(field, acc) do
-    acc <> "\n" <> stringify_field(field)
-  end
-
-  def stringify_field({field_name, value}) do
-    @field
-    |> String.replace(":field", to_string(field_name))
-    |> String.replace(":value", to_string(value))
+    :request
+    |> element(%{method: resource <> "." <> method}, keyword)
+    |> doc()
   end
 end
